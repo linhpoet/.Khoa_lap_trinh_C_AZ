58abd602496453bf2ba6786d1e511bf7c207c23b
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2021-06-25 01:08:36 +0700

    no message

--- Bai21_Count_Num_and_Reverse(HEX)/main.c
+++ Bai21_Count_Num_and_Reverse(HEX)/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+uint32_t u32Number = 0x00234;
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    printf("u8Length = %d\n", CountNumberHex(&u32Number));
+    printf("u32ReverseNumber = %d\n", ReverseNumberHex(&u32Number));
 }
+
+int CountNumberHex(uint32_t *pu32Number)
+{
+    uint8_t u8LengthHexNum = 8;
+    uint32_t u32TempNum = 0;
+
+    if(*pu32Number == 0)
+        return (0);
+
+    do
+    {
+        u32TempNum = (*pu32Number >> ((u8LengthHexNum - 1) * 4)) & 0xF;
+        u8LengthHexNum--;
+    }while (u32TempNum == 0);
+
+    return (u8LengthHexNum + 1);
+}
+
+int ReverseNumberHex(uint32_t *pu32Number)
+{
+    uint8_t u8LengthHexNum = CountNumberHex(pu32Number);
+    uint32_t u32TempNum = 0;
+    uint8_t u32TempCount = 0;
+
+    while(u32TempCount < u8LengthHexNum)
+    {
+        u32TempNum = (u32TempNum <<  4) | (((*pu32Number >> (u32TempCount * 4)) & 0xF));
+        u32TempCount++;
+    }
+    return (u32TempNum);
+
+}
+
+
+
+
