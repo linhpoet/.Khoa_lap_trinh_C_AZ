d5ca6dd4d5b7fe4c6fac43c7cefbd8c8ef836250
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2021-06-27 02:21:11 +0700

    no message

---
+++ Bai24_Typedef/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int LengthofFlash;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+void ReadFlash(LengthofFlash u32Length)
+{
+
+}
+
+void WriteFlash(LengthofFlash u32Length)
+{
+
+}
