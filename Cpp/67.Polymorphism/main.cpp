d83fe53d3cea90ad99e7fbef9b01dd69c0d01fcc
Le Quang Linh (MS/EAI22-AS) <Linh.LeQuang@vn.bosch.com>
2023-08-10 18:03:09 +0700

    update polymophism

--- Cpp/67.Polymorphism/main.cpp
+++ Cpp/67.Polymorphism/main.cpp
@@ -2,30 +2,45 @@
 
 using namespace std;
 
-class PhongTom
+class PhongTom                      //base class
 {
 public:
-    void SkillCoBan()
+    virtual void SkillCoBan()
     {
         cout <<"dam thuong"<<endl;
     }
+    int dam;
 };
 
-class CungThu: public PhongTom
+class CungThu: public PhongTom      //derived class
 {
 public:
     void SkillCoBan()
     {
          cout <<"ban cung"<<endl;
     }
+    int cung;
 };
 
+class GayThu: public PhongTom
+{
+public:
+    void SkillCoBan()
+    {
+        cout << "danh gay" << endl;
+    }
+};
 int main()
 {
-    PhongTom *pt1 = new PhongTom();
+    PhongTom *pt1 = new PhongTom;
     pt1->SkillCoBan();
-
-    PhongTom *pt2 = new CungThu();
+    PhongTom *pt2 = new CungThu;      //cấp phát ô nhớ kiểu CungThu gán vào con trỏ *pt2 kiểu PhongTom
     pt2->SkillCoBan();
+
+    /*Tuong tu nhu tren nhung khong phai la cap phat bo nho*/
+    PhongTom *pt3;
+    GayThu gaythu;
+    pt3 = &gaythu;
+    pt3->SkillCoBan();
     return 0;
 }
---
+++ Cpp/69.PolymophismWhyneed/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class PhongTom
+{
+public:
+    virtual void SkillCoBan()
+    {
+        cout << "lv < 10: skill co ban: dam thuong" <<endl;
+    }
+};
+
+class CungThu: public PhongTom
+{
+public:
+    void SkillCoBan()
+    {
+        cout<<"lv > 10: skill co ban: ban cung"<<endl;
+    }
+};
+
+class GayThu: public PhongTom
+{
+public:
+    void SkillCoBan()
+    {
+        cout <<"lv > 10: skill co ban: danh gay"<<endl;
+    }
+};
+
+void TungChieu(PhongTom *pnvgame, int level);
+bool isCungThu = false;
+int main()
+{
+    PhongTom *nvgame = new PhongTom;
+    int level = 1;
+    TungChieu(nvgame, level);
+
+    /*a long time later*/
+    level = 11;
+    isCungThu = true;
+    TungChieu(nvgame, level);
+
+    return 0;
+}
+
+void TungChieu(PhongTom *pnvgame, int level)
+{
+
+    if(level <= 10)
+    {
+        pnvgame->SkillCoBan();
+    }
+
+    else if(level >10)
+    {
+        if(isCungThu == true)
+        {
+            delete pnvgame;
+            PhongTom *pnvgame = new CungThu;
+        }
+
+        pnvgame->SkillCoBan();
+    }
+}
