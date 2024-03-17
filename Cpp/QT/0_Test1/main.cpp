ad2434227eac08d3f36c0ad5d28cbc47723f9471
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2024-03-17 22:50:54 +0700

    udpate Cpp Word

--- Cpp/QT/0_Test1/main.cpp
+++ Cpp/QT/0_Test1/main.cpp
@@ -1,38 +1,19 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <QDebug>
 
-class test : public QObject
+using namespace std;
+int main()
 {
-    //When you have your class defined in .cpp file instead of .h file moc fails to process it properly.
-    //the moc tool reads a C++ header file
-    //if you have O_OBJECT in main.cpp, add #include "main.moc" at the end of the file:
-    Q_OBJECT
-public:
-    void dostuff();
-    static void doOtherStuff();
-};
+    int a;
+    cout <<"nhap:";
+    cin >> a;
+    cout <<a << endl;
 
-void test::dostuff()
-{
-    qInfo() << "do stuff" ;
+
+    qInfo() << "information";
+    qDebug() << "Debug";
+    qCritical() << "Critical";
+    //qFatal("Test Crash");
+
+    return 0;
 }
-test globalObject;
-void test::doOtherStuff()
-{
-    qInfo() << "static fc do other stuff";
-}
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    test localObject;
-    qInfo() << &localObject;
-    qInfo() << &globalObject;
-    test t;
-    t.dostuff();
-    t.doOtherStuff();
-
-    test::doOtherStuff();
-
-    return a.exec();
-}
-#include "main.moc"
